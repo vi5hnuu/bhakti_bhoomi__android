2ea11132af762e09e8e96329b3bce39d996f4c67
vi5hnu <kumarvishnu1619@gmail.com>
2024-02-03 17:24:21 +0530

    mantra screens design completed

--- lib/pages/mantra/MantraInfoScreen.dart
+++ lib/pages/mantra/MantraInfoScreen.dart
@@ -1,5 +1,6 @@
 import 'package:bhakti_bhoomi/routing/routes.dart';
 import 'package:bhakti_bhoomi/state/mantra/mantra_bloc.dart';
+import 'package:bhakti_bhoomi/widgets/RoundedListTile.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -27,20 +28,31 @@ class _MantraInfoScreenState extends State<MantraInfoScreen> {
     return BlocBuilder<MantraBloc, MantraState>(
       buildWhen: (previous, current) => previous != current,
       builder: (context, state) {
-        final mantraInfo = state.allMantraInfo;
+        final allMantraInfo = state.allMantraInfo;
         return Scaffold(
             appBar: AppBar(
-              title: Text('Mantra'),
+              title: Text(
+                widget.title,
+                style: TextStyle(color: Colors.white, fontFamily: "Kalam", fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              backgroundColor: Theme.of(context).primaryColor,
+              iconTheme: const IconThemeData(color: Colors.white),
             ),
-            body: mantraInfo != null
-                ? Column(
-                    children: mantraInfo.entries
-                        .map((e) => InkWell(
-                              onTap: () => GoRouter.of(context).pushNamed(Routing.mantra, pathParameters: {'mantraId': e.key}),
-                              child: Text(e.value.title),
-                            ))
-                        .toList(),
-                  )
+            body: allMantraInfo != null
+                ? ListView.builder(
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+                    itemCount: allMantraInfo.length,
+                    itemBuilder: (context, index) {
+                      final mantraInfo = allMantraInfo.entries.toList()[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: RoundedListTile(
+                          onTap: () => GoRouter.of(context).pushNamed(Routing.mantra, pathParameters: {'mantraId': mantraInfo.key}),
+                          itemNo: index + 1,
+                          text: mantraInfo.value.title,
+                        ),
+                      );
+                    })
                 : state.error != null
                     ? Center(child: Text(state.error!))
                     : Center(child: CircularProgressIndicator()));
--- lib/pages/mantra/MantraScreen.dart
+++ lib/pages/mantra/MantraScreen.dart
@@ -1,3 +1,4 @@
+import 'package:bhakti_bhoomi/models/mantra/MantraModel.dart';
 import 'package:bhakti_bhoomi/state/mantra/mantra_bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -29,15 +30,133 @@ class _MantraScreenState extends State<MantraScreen> {
         final mantra = state.getMantraById(mantraId: widget.mantraId);
         return Scaffold(
             appBar: AppBar(
-              title: Text('Rigveda'),
+              title: Text(
+                state.isLoading || state.error != null || mantra == null ? 'Mantra' : mantra.title,
+                style: TextStyle(color: Colors.white, fontFamily: "Kalam", fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              backgroundColor: Theme.of(context).primaryColor,
+              iconTheme: IconThemeData(color: Colors.white),
             ),
             body: mantra != null
-                ? Text(mantra.title)
+                ? SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: _getMantras(mantras: mantra.mantras),
+                      ),
+                    ),
+                  )
                 : state.error != null
                     ? Center(child: Text(state.error!))
                     : Center(child: CircularProgressIndicator()));
       },
     );
+  }
+
+  List<Widget> _getMantras({required List<MantraModel>? mantras, bool inner = false}) {
+    if (mantras == null) return List.empty();
+    return mantras
+        .map((mantra) => Padding(
+              padding: EdgeInsets.only(left: 5 + (inner ? 24 : 0), right: 5, top: 5, bottom: 5),
+              child: ExpansionTile(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  collapsedBackgroundColor: Theme.of(context).primaryColor,
+                  childrenPadding: const EdgeInsets.all(5),
+                  collapsedTextColor: Colors.white,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                  collapsedShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                  title: Text(mantra.title, style: TextStyle(color: Colors.white)),
+                  textColor: Colors.white,
+                  iconColor: Colors.white,
+                  collapsedIconColor: Colors.white,
+                  expandedAlignment: Alignment.centerLeft,
+                  children: [
+                    if (_hasDescription(mantra.description))
+                      Padding(
+                        padding: EdgeInsets.only(left: 12 + (inner ? 24 : 0)),
+                        child: _getDescription(mantra.description),
+                      ),
+                    if (_hasDescription(mantra.description) && mantra.translations != null) const SizedBox(height: 12),
+                    if (mantra.translations != null)
+                      Padding(
+                        padding: EdgeInsets.only(left: 12 + (inner ? 24 : 0)),
+                        child: _getTransLations(mantra.translations!),
+                      ),
+                    ..._getMantras(mantras: mantra.subMantras, inner: true)
+                  ]),
+            ))
+        .toList();
+  }
+
+  _getDescription(Map<String, String> description) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Description', style: const TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)),
+        SizedBox(height: 8),
+        if (description['hi'] != null && description['hi']!.isNotEmpty) ...[
+          Text(
+            'Hindi : ',
+            style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 7),
+          Text(
+            description['hi']!,
+            style: TextStyle(color: Colors.white),
+          )
+        ],
+        if (_hasDescription(description)) const SizedBox(height: 12),
+        if (description['eng'] != null && description['eng']!.isNotEmpty) ...[
+          Text(
+            'English : ',
+            style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 7),
+          Text(
+            description['eng']!,
+            style: TextStyle(color: Colors.white),
+          )
+        ]
+      ],
+    );
+  }
+
+  bool _hasDescription(Map<String, String> description) {
+    return (description['hi'] != null && description['hi']!.isNotEmpty) || (description['eng'] != null && description['eng']!.isNotEmpty);
+  }
+
+  _getTransLations(Map<String, List<String>> translations) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Translations', style: const TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)),
+        SizedBox(height: 8),
+        if (translations['hi'] != null && translations['hi']!.isNotEmpty) ...[
+          Text(
+            'Hindi : ',
+            style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 7),
+          Text(
+            translations['hi']!.join(''),
+            style: TextStyle(color: Colors.white),
+          )
+        ],
+        const SizedBox(height: 12),
+        if (translations['eng'] != null && translations['eng']!.isNotEmpty) ...[
+          Text(
+            'English : ',
+            style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 7),
+          Text(
+            translations['eng']!.join(''),
+            style: TextStyle(color: Colors.white),
+          )
+        ]
+      ],
+    );
+    ;
   }
 
   @override
